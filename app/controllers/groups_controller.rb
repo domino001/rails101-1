c70a1e291b83dd9d853b3316cb4c7092aa73d090
nailo <nailocwc@gmail.com>
2016-12-25 17:31:43 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -56,13 +56,6 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "Group deleted"
   end
 
-    def destroy
-      @group = Group.find(params[:id])
-      @group.destroy
-      flash[:alert] = "Group deleted"
-      redirect_to groups_path
-    end
-
   private
 
   def group_params
